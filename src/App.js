173cca25f435482d33082776c2826a9ac0aa007c
gykoh42 <karmakoh42@naver.com>
2024-04-08 12:00:00 +0900

    feat: TodoList 페이지 구현 및 검색 기능 구현중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
+import AddTodo from "./AddTodo";
+import RadioGroup from "./Radio";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { value: "코딩하기", completed: false },
+    { value: "밥먹기", completed: false },
+    { value: "산책하기", completed: false },
+  ]);
+  const [radioState, setRadioState] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, { value: newTodo, completed: false }]);
+  };
+
+  const removeTodo = (removeIdx) => {
+    setTodos(todos.filter((val, index) => index !== removeIdx));
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo, index) => {
+        if (id === index) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const changeRadioState = (newState) => {
+    setRadioState(newState);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    // if (radioState === "All") return true;
+    // if (radioState === "In Progress") return !todo.completed;
+    // if (radioState === "Completed") return todo.completed;
+    return todo.value.includes(searchTerm);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <AddTodo addTodo={addTodo} />
+      {/* <input
+        type="text"
+        placeholder="검색"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      /> */}
+      <RadioGroup changeRadioState={changeRadioState} />
+      <TodoList
+        todos={filteredTodos}
+        removeTodo={removeTodo}
+        toggleCompleted={toggleCompleted}
+      />
     </div>
   );
 }
