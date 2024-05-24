5e45fd05d990f04be42b074c6db4606b981cd0c2
gykoh42 <karmakoh42@naver.com>
2024-05-25 05:22:43 +0900

    feat: Radio.js 에 css 스타일 적용 및 호버 색상 변경

--- src/Radio.js
+++ src/Radio.js
@@ -1,27 +1,80 @@
+import styled from "styled-components";
+
 const RadioGroup = ({ changeRadioState }) => {
   return (
-    <>
-      <input
-        type="radio"
-        name="radio"
-        defaultChecked
-        onChange={() => changeRadioState("All")}
-      />
-      <label>All</label>
-      <input
-        type="radio"
-        name="radio"
-        onChange={() => changeRadioState("In Progress")}
-      />
-      <label>In Progress</label>
-      <input
-        type="radio"
-        name="radio"
-        onChange={() => changeRadioState("Completed")}
-      />
-      <label>Completed</label>
-    </>
+    <RadioGroupStyled>
+      <RadioLabel className="radio-label all">
+        <RadioInput
+          type="radio"
+          name="radio"
+          defaultChecked
+          onChange={() => changeRadioState("All")}
+        />
+        <RadioLabelText>All</RadioLabelText>
+      </RadioLabel>
+      <RadioLabel className="radio-label in-progress">
+        <RadioInput
+          type="radio"
+          name="radio"
+          onChange={() => changeRadioState("In Progress")}
+        />
+        <RadioLabelText>In Progress</RadioLabelText>
+      </RadioLabel>
+      <RadioLabel className="radio-label completed">
+        <RadioInput
+          type="radio"
+          name="radio"
+          onChange={() => changeRadioState("Completed")}
+        />
+        <RadioLabelText>Completed</RadioLabelText>
+      </RadioLabel>
+    </RadioGroupStyled>
   );
 };
+
+const RadioGroupStyled = styled.div`
+  display: flex;
+  margin-top: 15px;
+  margin-bottom: 20px;
+`;
+
+const RadioLabel = styled.label`
+  position: relative;
+  font-size: 14px;
+  font-weight: 800;
+  border: 2px solid #000;
+  display: block;
+  cursor: pointer;
+  padding: 8px 16px;
+  background-color: #fff;
+  transition: background-color 0.3s ease;
+  margin: 0 5px;
+  width: auto;
+
+  &:hover,
+  &:focus-within {
+    background-color: #8b0000;
+    color: white;
+  }
+`;
+
+const RadioInput = styled.input`
+  opacity: 0;
+  position: absolute;
+
+  &:checked {
+    & + span {
+      background-color: transparent;
+    }
+    & ~ .radio-label {
+      background-color: #8b0000;
+      color: white;
+    }
+  }
+`;
+
+const RadioLabelText = styled.span`
+  display: block;
+`;
 
 export default RadioGroup;
